4d94dc218ec50dc4a5c0ee6052674c8e97c0f5c2
Xsozm <hazem29778865@gmail.com>
2020-01-15 07:48:18 +0100

    UseController

--- controllers/PostersController.js
+++ controllers/PostersController.js
@@ -1,25 +1,102 @@
-export const calculateCost = (req, res) => {
-  const body = req.body; //input
-  const orders = [];
-  for (let packCode in body) {
-    const packsLimits = prodcuts[packCode].Packs.map(pack => pack.limit);
-    const packsPrices = prodcuts[packCode].Packs.map(pack => pack.price);
+import knex from '../data/db';
+import { addDays } from '../helpers/Datehelper';
 
-    const targetValue = body[packCode];
-    const { totalNumberOfPacks, optimalPacks, totalCost } = getOptimalCost(
-      packsLimits,
-      targetValue,
-      packsPrices
-    );
-
-    orders.push({
-      order: `${body[packCode]} ${packCode}`,
-      totalCost,
-      optimalPacks,
+export const getAllPrinters = (req, res) => {
+  knex
+    .from('printers')
+    .select('name', 'phone', 'email', 'country_id')
+    .then(ans => {
+      res.status(200).send(ans);
     });
-  }
-  const responseObject = {
-    Orders: orders,
-  };
-  res.status(200).json(responseObject);
+};
+
+export const getAllPrintersWithCountryInfo = (req, res) => {
+  knex('printers')
+    .join('countries', 'printers.country_id', '=', 'countries.id')
+    .select(
+      'printers.name',
+      'printers.phone',
+      'printers.email',
+      'countries.name as countryName'
+    )
+    .then(ans => {
+      res.status(200).send(ans);
+    });
+};
+
+export const getAPrinterInfo = (req, res) => {
+  const printer_id = 1;
+  knex('printers')
+    .where('country_id', '=', printer_id)
+    .select('name', 'phone', 'email')
+    .then(ans => {
+      res.status(200).send(ans);
+    });
+};
+
+export const makeOrder = (req, res) => {
+  const product_id = 1; // product_id
+  const destination = 1; // country_id
+
+  knex('printers')
+    .join('product_printers', 'printers.id', '=', 'product_printers.printer_id')
+    .where('product_printers.product_id', '=', product_id)
+    .join(
+      'delievery_times',
+      'delievery_times.source',
+      '=',
+      'printers.country_id'
+    )
+    .where('delievery_times.destination', '=', destination)
+    .orderBy('delievery_times.period', 'asc')
+    .limit(1)
+    .then(ans => {
+      if (!ans) {
+        res.status(201).send("We Can't do this Order");
+      }
+      knex('orders')
+        .insert(
+          {
+            product_id: product_id,
+            printer_id: ans[0].printer_id,
+            country_id: destination,
+            toBeDelivered_At: addDays(ans[0].period).toString(),
+            name: 'product cool',
+          },
+          'order'
+        )
+        .then(order => {
+          res
+            .status(200)
+            .send({ response: 'Order Created Successfully !', order });
+        });
+    });
+};
+
+export const getPrinterOrders = (req, res) => {
+  const printer_id = 1;
+  knex('printers')
+    .where('printers.id', '=', printer_id)
+    .join('orders', 'orders.printer_id', '=', 'printers.id')
+    .join('products', 'orders.product_id', '=', 'products.id')
+    .join('countries', 'countries.id', '=', 'orders.country_id')
+    .join(
+      'countries AS printerCountry',
+      'printerCountry.id',
+      '=',
+      'printers.country_id'
+    )
+
+    .select(
+      'orders.name as OrderName',
+      'orders.toBeDelivered_At as DeilveredAt',
+      'printers.email as PrinterEmail',
+      'products.name as ProductOrdered',
+      'printers.name as PrinterName',
+      'countries.name as Destination',
+      'printerCountry.name as shippedFrom'
+    )
+    .then(ans => {
+      res.status(200).send(ans);
+    });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import {
+  getAllPrinters,
+  getAllPrintersWithCountryInfo,
+  makeOrder,
+  getPrinterOrders,
+} from '../controllers/PostersController';
 const router = express.Router();
-import knex from '../data/db';
-import { addDays } from '../helpers/Datehelper';
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.send('Hello World');
@@ -17,106 +22,17 @@ router.get('/test', (req, res) => {
 });
 
 // get all printers
-router.get('/printers', (req, res) => {
-  knex
-    .from('printers')
-    .select('name', 'phone', 'email', 'country_id')
-    .then(ans => {
-      res.status(200).send(ans);
-    });
-});
+router.get('/printers', getAllPrinters);
 
 // get all printers with Info
-router.get('/printersInfo', (req, res) => {
-  knex('printers')
-    .join('countries', 'printers.country_id', '=', 'countries.id')
-    .select(
-      'printers.name',
-      'printers.phone',
-      'printers.email',
-      'countries.name as countryName'
-    )
-    .then(ans => {
-      res.status(200).send(ans);
-    });
-});
+router.get('/printersInfo', getAllPrintersWithCountryInfo);
 
 // get all printers in a country with id=1 for example
-router.get('/getPrintersInCountry', (req, res) => {
-  knex('printers')
-    .where('country_id', '=', '1')
-    .select('name', 'phone', 'email')
-    .then(ans => {
-      res.status(200).send(ans);
-    });
-});
+router.get('/getPrintersInCountry', getAPrinterInfo);
 
 // get orders for specific printer
-router.get('/getPrinterOrders', (req, res) => {
-  const printer_id = 1;
-  knex('printers')
-    .where('printers.id', '=', printer_id)
-    .join('orders', 'orders.printer_id', '=', 'printers.id')
-    .join('products', 'orders.product_id', '=', 'products.id')
-    .join('countries', 'countries.id', '=', 'orders.country_id')
-    .join(
-      'countries AS printerCountry',
-      'printerCountry.id',
-      '=',
-      'printers.country_id'
-    )
-
-    .select(
-      'orders.name as OrderName',
-      'orders.toBeDelivered_At as DeilveredAt',
-      'printers.email as PrinterEmail',
-      'products.name as ProductOrdered',
-      'printers.name as PrinterName',
-      'countries.name as Destination',
-      'printerCountry.name as shippedFrom'
-    )
-    .then(ans => {
-      res.status(200).send(ans);
-    });
-});
+router.get('/getPrinterOrders', getPrinterOrders);
 
 // create order
-router.get('/makeOrder', (req, res) => {
-  const product_id = 1; // product_id
-  const destination = 1; // country_id
-
-  knex('printers')
-    .join('product_printers', 'printers.id', '=', 'product_printers.printer_id')
-    .where('product_printers.product_id', '=', product_id)
-    .join(
-      'delievery_times',
-      'delievery_times.source',
-      '=',
-      'printers.country_id'
-    )
-    .where('delievery_times.destination', '=', destination)
-    .orderBy('delievery_times.period', 'asc')
-    .limit(1)
-    .then(ans => {
-      if (!ans) {
-        res.status(201).send("We Can't do this Order");
-      }
-      knex('orders')
-        .insert(
-          {
-            product_id: product_id,
-            printer_id: ans[0].printer_id,
-            country_id: destination,
-            toBeDelivered_At: addDays(ans[0].period).toString(),
-            name: 'product cool',
-          },
-          'order'
-        )
-        .then(order => {
-          res
-            .status(200)
-            .send({ response: 'Order Created Successfully !', order });
-        });
-    });
-});
+router.get('/makeOrder', makeOrder);
 export default router;
